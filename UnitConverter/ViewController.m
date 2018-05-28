03f58ba791347babfbfaf0e68aec729278175439
Duc Vu <de.yd315@gmail.com>
2018-05-28 11:20:07 -0500

    completed

--- UnitConverter/ViewController.m
+++ UnitConverter/ViewController.m
@@ -7,13 +7,59 @@
 //
 
 #import "ViewController.h"
+// convert from inches to feet
+double convertInchesToFeet(double unitOneValue){
+    double unitTwoValue = unitOneValue / 12;
+    return unitTwoValue;
+}
+
+// convert from inches to yards
+double convertInchesToYards(double unitOneValue){
+    double unitTwoValue = unitOneValue / 36;
+    return unitTwoValue;
+}
+
+// convert from inches to miles
+double convertInchesToMiles(double unitOneValue){
+    double unitTwoValue = unitOneValue / 63360;
+    return unitTwoValue;
+}
 
 @interface ViewController ()
-
-@end
+    @property (weak, nonatomic) IBOutlet UITextField *inputField;
+    @property (weak, nonatomic) IBOutlet UISegmentedControl *segmenController;
+    @property (weak, nonatomic) IBOutlet UILabel *outputField;
+    
+    @property (weak, nonatomic) IBOutlet UILabel *outputLableField;
+    @end
 
 @implementation ViewController
-
+- (IBAction)updateButton:(id)sender {
+    NSMutableString *buf = [NSMutableString new];
+    
+    double userInput = [self.inputField.text doubleValue];
+    
+    // segment 1 - convert from inches to feet
+    if(self.segmenController.selectedSegmentIndex == 0){
+        double unitTwoValue = convertInchesToFeet(userInput);
+        [buf appendString: [@(unitTwoValue) stringValue]];
+    }
+    
+    // segment 2 - convert from inches to yards
+    else if (self.segmenController.selectedSegmentIndex == 1){
+        double unitTwoValue = convertInchesToYards(userInput);
+        [buf appendString: [@(unitTwoValue) stringValue]];
+    }
+    
+    // segment 3 - convert from inches to miles
+    else {
+        double unitTwoValue = convertInchesToMiles(userInput);
+        [buf appendString: [@(unitTwoValue) stringValue]];
+    }
+    
+    self.outputLableField.text = buf;
+}
+    
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
